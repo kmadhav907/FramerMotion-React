e651e77883732d8843364ddbf92e5d7c2a6ca446
kmadhav907 <kmadhav907@gmail.com>
2021-04-06 20:30:09 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+  return (
+    <motion.div
+      className='home container'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
+      <Link to='/base'>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Home;
